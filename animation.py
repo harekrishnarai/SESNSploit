3d153697ce2e0a74e2834f6a754e3ba69aababf8
Hare Krishna Rai <63994966+harekrishnarai@users.noreply.github.com>
2025-02-11 20:07:56 +0000

    deleted unnecesary files

--- animation.py
+++
@@ -1,7 +0,0 @@
-import time
-
-def loading_animation():
-    animations = ['[■□□□□□□□□□]', '[■■□□□□□□□□]', '[■■■□□□□□□□]', '[■■■■□□□□□□]', '[■■■■■□□□□□]', '[■■■■■■□□□□]', '[■■■■■■■□□□]', '[■■■■■■■■□□]', '[■■■■■■■■■□]', '[■■■■■■■■■■]']
-    for i in range(20):
-        print(f"\r{animations[i % len(animations)]}", end='', flush=True)
-        time.sleep(0.1)
